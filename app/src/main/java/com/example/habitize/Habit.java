3ddf11156d2aa3bde1c51712fc4966818eb6e755
sfedykua <stasfedyk1911@gmail.com>
2021-10-22 15:52:52 -0600

    created habit calss

---
+++ app/src/main/java/com/example/habitize/Habit.java
@@ -0,0 +1,31 @@
+package com.example.habitize;
+
+public class Habit {
+    private String name;
+    private String description;
+    private int streak;
+    private Boolean isDone;
+    // TODO: We need image + date functionality
+
+    // TODO: Add image + date constructors
+    public void Habit(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+    public void incrementStreak(){
+        this.streak++;
+    }
+
+    // TODO: should this trigger automatically if a day goes by without incrementstreak?
+    public void endStreak(){
+        this.streak = 0;
+    }
+    public void resetTask(){
+        this.isDone = false;
+    }
+    public void doTask(){
+        this.isDone = true;
+    }
+
+
+}
