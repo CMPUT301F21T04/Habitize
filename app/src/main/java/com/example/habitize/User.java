7bb35ebf70d27697c002949adf93719f352417e1
khalida1wwin <khalida1w@gmail.com>
2021-10-22 18:41:35 -0600

    added the following classes:user,login and SingUp

---
+++ app/src/main/java/com/example/habitize/User.java
@@ -0,0 +1,13 @@
+package com.example.habitize;
+
+public class User {
+    private String UserName;
+    private String Password;
+    private String FirstName;
+    private String LastName;
+    private String Following;
+    private String Followers;
+    private String Progress;
+    private String UserHabits;
+    private String Points;
+}
