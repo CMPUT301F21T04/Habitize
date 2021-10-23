573dd49a469e2f0bc0252bb9ed8b1042027e9713
sfedykua <stasfedyk1911@gmail.com>
2021-10-22 22:35:29 -0600

    started on customList, AllHabits UI

---
+++ app/src/test/java/com/example/habitize/ListCityTest.java
@@ -0,0 +1,22 @@
+package com.example.tests;
+
+import androidx.core.widget.TextViewCompat;
+
+import com.example.habitize.Habit;
+import com.example.habitize.HabitList;
+
+public class ListCityTest {
+    private HabitList testHabitList(){
+        HabitList habitlist = new HabitList();
+        return habitlist;
+    }
+    private Habit testHabit(){
+        Habit habit = new Habit();
+        return habit;
+    }
+
+
+
+
+
+}
