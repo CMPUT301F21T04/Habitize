ec81c33bd890b03350ccc76089765744f698812c
Shanemel Asuncion <shanemelasuncion@gmail.com>
2021-10-23 02:02:18 -0600

    Register Screen
    I updated the User.java by adding getters & setters and a constructor for abstraction. I also added the database in RegisterActivity.java and added the header made by Khalid to  register_activity.xml

--- app/src/main/java/com/example/habitize/User.java
+++ app/src/main/java/com/example/habitize/User.java
@@ -10,4 +10,94 @@ public class User {
     private String Progress;
     private String UserHabits;
     private String Points;
+
+
+    User(String UserName,String Password,String FirstName,
+         String LastName,String Following, String Followers,
+         String Progress, String UserHabits, String Points){
+        this.UserName = UserName;
+        this.Password = Password;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Following = Following;
+        this.Followers = Followers;
+        this.Progress = Progress;
+        this.UserHabits = UserHabits;
+        this.Points = Points;
+    }
+
+    //Getters and Setters
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getFollowing() {
+        return Following;
+    }
+
+    public void setFollowing(String following) {
+        Following = following;
+    }
+
+    public String getFollowers() {
+        return Followers;
+    }
+
+    public void setFollowers(String followers) {
+        Followers = followers;
+    }
+
+    public String getProgress() {
+        return Progress;
+    }
+
+    public void setProgress(String progress) {
+        Progress = progress;
+    }
+
+    public String getUserHabits() {
+        return UserHabits;
+    }
+
+    public void setUserHabits(String userHabits) {
+        UserHabits = userHabits;
+    }
+
+    public String getPoints() {
+        return Points;
+    }
+
+    public void setPoints(String points) {
+        Points = points;
+    }
+
 }
