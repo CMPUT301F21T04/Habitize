c868e17364575aaf7ac8859d8818898d525003ed
sfedykua <stasfedyk1911@gmail.com>
2021-10-24 15:10:56 -0600

    worked on customAdapter

--- app/src/main/java/com/example/habitize/CustomAdapter.java
+++ app/src/main/java/com/example/habitize/CustomAdapter.java
@@ -1,2 +1,32 @@
-package com.example.habitize;public class CustomAdapter {
+package com.example.habitize;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter<Habit> {
+
+    private ArrayList<Habit> habits;
+    private Context context;
+
+    public CustomAdapter(Context context, ArrayList<Habit> habits){
+        super(context,0,habits);
+        this.habits = habits;
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+
+    }
+
 }
--- app/src/test/java/com/example/habitize/ListCityTest.java
+++ app/src/test/java/com/example/habitize/ListCityTest.java
@@ -1,9 +1,13 @@
-package com.example.tests;
+package com.example.habitize;
+
+import static org.junit.Assert.assertEquals;
 
 import androidx.core.widget.TextViewCompat;
 
 import com.example.habitize.Habit;
 import com.example.habitize.HabitList;
+
+import org.junit.Test;
 
 public class ListCityTest {
     private HabitList testHabitList(){
@@ -13,6 +17,26 @@ public class ListCityTest {
     private Habit testHabit(){
         Habit habit = new Habit();
         return habit;
+    }
+
+    @Test
+    public void addHabitTest(){
+        HabitList myList = testHabitList();
+        Habit myHabit = testHabit();
+
+        myList.addHabit(myHabit);
+        assertEquals(true,myList.contains(myHabit));
+    }
+    @Test
+    public void deleteHabitTest(){
+        HabitList myList = testHabitList();
+        Habit myHabit = testHabit();
+
+        myList.addHabit(myHabit);
+        assertEquals(true,myList.contains(myHabit));
+        myList.deleteHabit(myHabit);
+        assertEquals(false,myList.contains(myHabit));
+
     }
 
 
