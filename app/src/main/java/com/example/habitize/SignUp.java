6475c5d260453e17e52f34c99c99086ba9d58a2f
khalida1wwin <khalida1w@gmail.com>
2021-10-24 00:55:43 -0600

    made signup class

--- app/src/main/java/com/example/habitize/SignUp.java
+++ app/src/main/java/com/example/habitize/SignUp.java
@@ -1,14 +1,20 @@
 package com.example.habitize;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
@@ -27,16 +33,40 @@ public class SignUp extends AppCompatActivity {
         EditText email = findViewById(R.id.email);
         ProgressBar progressBar = findViewById(R.id.progressBar);
 
+
+        if (fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+
         fAuth = FirebaseAuth.getInstance();
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String inputEmail = email.getText().toString().trim();
+                String inputPassowrd = email.getText().toString().trim();
 
                 if (TextUtils.isEmpty(inputEmail)){
                     email.setError("Enter an email please!");
                     return;
                 }
+                if (TextUtils.isEmpty(inputPassowrd)){
+                    password.setError("Passsword should be greater than 8 characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(inputEmail,inputPassowrd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(SignUp.this,"You have made an account successfully!",Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(SignUp.this,"Something Wrong!" + task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
             }
         });
 
