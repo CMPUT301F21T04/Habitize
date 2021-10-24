8cebb00576be4e482d54b7cd54ac8c3fc1498c09
sfedyk <stasfedyk1911@gmail.com>
2021-10-24 17:33:16 -0600

    implementing intent testing

--- app/src/main/java/com/example/habitize/AllHabitsActivity.java
+++ app/src/main/java/com/example/habitize/AllHabitsActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class AllHabitsActivity extends AppCompatActivity {
+    private HabitList habitList;
+    private ArrayList<Habit> dataList;
+    private CustomAdapter habitAdapter;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
