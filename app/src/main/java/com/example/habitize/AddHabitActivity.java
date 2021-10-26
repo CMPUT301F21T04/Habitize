57f5086e1e2a10aada108709fd1c1a76cde8193c
Rachel <45080402+rachelraso@users.noreply.github.com>
2021-10-26 01:33:48 -0600

    New Habit Screen XML done

---
+++ app/src/main/java/com/example/habitize/AddHabitActivity.java
@@ -0,0 +1,18 @@
+package com.example.habitize;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddHabitActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_habit);
+
+
+
+
+
+    }
+}
