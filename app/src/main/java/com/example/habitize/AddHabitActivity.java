44d5c5bff6c06e841a015d1f5e35483d821065d4
Rachel <45080402+rachelraso@users.noreply.github.com>
2021-10-26 02:13:46 -0600

    Started on AddHabitActivity backend

--- app/src/main/java/com/example/habitize/AddHabitActivity.java
+++ app/src/main/java/com/example/habitize/AddHabitActivity.java
@@ -1,14 +1,35 @@
 package com.example.habitize;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddHabitActivity extends AppCompatActivity {
+    private EditText title;
+    private EditText description;
+    private EditText startDate;
+    private Button monday;
+    private Button tuesday;
+    private Button wednesday;
+    private Button thursday;
+    private Button friday;
+    private Button saturday;
+    private Button sunday;
+    private Switch geolocation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_habit);
+
+        //TO DO: make a header w/ title - NEW HABIT - perhaps textview trick
+
+
+
 
 
 
