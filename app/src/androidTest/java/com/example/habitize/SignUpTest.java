7e3456e5db051623544eeaa8ae40e6c9b33c7fd6
khalida1wwin <khalida1w@gmail.com>
2021-10-27 17:17:49 -0600

    Update signup

--- app/src/androidTest/java/com/example/habitize/SignUpTest.java
+++ app/src/androidTest/java/com/example/habitize/SignUpTest.java
@@ -13,6 +13,13 @@ import org.junit.Rule;
 public class SignUpTest {
     @Rule
     public ActivityScenarioRule<SignUp> rule = new ActivityScenarioRule<SignUp>(SignUp.class);
+
+    String testFirstName = "Rick";
+    String testLastName = "Grimes";
+    String testEmail = "rick0grimes301@gmail.com";
+    String testUsername = "Rick";
+    String testPassword = "12345678";
+    String testConPassword = "12345678";
     
     
 
--- app/src/main/java/com/example/habitize/SignUp.java
+++ app/src/main/java/com/example/habitize/SignUp.java
@@ -36,6 +36,7 @@ public class SignUp extends AppCompatActivity {
         EditText lastName = findViewById(R.id.lastName);
         EditText username = findViewById(R.id.userName);
         EditText password = findViewById(R.id.password);
+        EditText conPassword = findViewById(R.id.conPassword);
         EditText email = findViewById(R.id.email);
         ProgressBar progressBar = findViewById(R.id.progressBar2);
 
@@ -65,32 +66,40 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String inputEmail = email.getText().toString().trim();
-                String inputPassowrd = password.getText().toString().trim();
-
+                String inputPassword = password.getText().toString().trim();
+                String inputConPass = conPassword.getText().toString().trim();
 
                 if (TextUtils.isEmpty(inputEmail)){
                     email.setError("Enter an email please!");
                     return;
                 }
-                if (TextUtils.isEmpty(inputPassowrd)){
+                if (TextUtils.isEmpty(inputPassword)){
                     password.setError("Please enter a password!");
                     return;
                 }
-                if (inputPassowrd.length() < 8){
+                if (inputPassword.length() < 8){
                     password.setError("Passsword should be greater than 8 characters");
                     return;
                 }
-                
+
+                if (inputPassword != inputConPass){
+                    conPassword.setError("The passwords are not the same!");
+                    return;
+                }
+
+
+
+
                 progressBar.setVisibility(View.VISIBLE);
                 // TODO: the startActivity here might mess with NAVCONTROLLER
-                fAuth.createUserWithEmailAndPassword(inputEmail,inputPassowrd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                fAuth.createUserWithEmailAndPassword(inputEmail,inputPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             Toast.makeText(SignUp.this,"You have made an account successfully!",Toast.LENGTH_LONG).show();
                             // we created an account. Lets set up the user stuff
                             // TODO: Put legit values here later. Just filling the constructor because I need it to work
-                            User newUser = new User(inputEmail,inputPassowrd,inputEmail,inputEmail,new ArrayList<User>(),new ArrayList<User>(),0,new ArrayList<Habit>(),0);
+                            User newUser = new User(inputEmail,inputPassword,inputEmail,inputEmail,new ArrayList<User>(),new ArrayList<User>(),0,new ArrayList<Habit>(),0);
                             HashMap<String,User> userData = new HashMap<>();
                             userData.put("User",newUser);
                             users.document(inputEmail).set(userData); // user data gets stored.
