2ef368d2726e6df187981fb6eea406c0fee70e4a
khalida1wwin <khalida1w@gmail.com>
2021-10-27 16:44:36 -0600

    Updated Tests

--- app/src/androidTest/java/com/example/habitize/SignUpTest.java
+++ app/src/androidTest/java/com/example/habitize/SignUpTest.java
@@ -5,38 +5,18 @@ import android.app.Activity;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-import com.robotium.solo.Solo;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
+
 public class SignUpTest {
-    private Solo solo;
     @Rule
-    public ActivityTestRule<SignUp> rule = new ActivityTestRule<>(SignUp.class, true, true);
-    @Before
-    public void setUp() throws Exception{
-        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
-    }
-    /**
-     * Check whether activity correctly switched
-     */
-    @Test
-    public void checkActivity() throws Exception {
-        solo.clickOnButton("REGISTER"); //Click ADD CITY Button
-        Activity activity = rule.getActivity();
-        solo.assertCurrentActivity("Wrong",SignUp.class);
-    }
-
-    @After
-    public void tearDown() throws Exception{
-        solo.finishOpenedActivities();
+    public ActivityScenarioRule<SignUp> rule = new ActivityScenarioRule<SignUp>(SignUp.class);
+    
+    
 
 
-    }
 }
 
 
