76e0f7ba0572072f31a92ce56eb07ae2d1410010
Shanemel Asuncion <shanemelasuncion@gmail.com>
2021-10-27 18:20:16 -0600

    Update
    Added the functionality for image picker in the AddHabitImage. Added the buttons in AddHabitActivity and AddHabitLocation. Fixed the responsiveness of the habitize header in the login and singup.

--- app/src/main/java/com/example/habitize/AddHabitActivity.java
+++ app/src/main/java/com/example/habitize/AddHabitActivity.java
@@ -1,6 +1,8 @@
 package com.example.habitize;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -19,19 +21,33 @@ public class AddHabitActivity extends AppCompatActivity {
     private Button saturday;
     private Button sunday;
     private Switch geolocation;
+    private Button imageBtn;
+    private Button locationBtn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_habit);
+        setContentView(R.layout.activity_add_habit_info);
+
+        imageBtn = findViewById(R.id.addImage);
+        locationBtn = findViewById(R.id.addLocation);
 
         //TO DO: make a header w/ title - NEW HABIT - perhaps textview trick
 
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddHabitImage.class));             // redo intent handling
+            }
+        });
 
-
-
-
+        locationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddHabitLocation.class));          // redo intent handling
+            }
+        });
 
 
 
