993bf802028b0b4e1465c1b9c8ce76896f905d0b
kamillahhasham <kamillah@ualberta.ca>
2021-10-28 18:44:43 -0600

    Working on creating the custom list

---
+++ app/src/main/java/com/example/habitize/CustomListOfExistingFollowers.java
@@ -0,0 +1,14 @@
+package com.example.habitize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomListOfExistingFollowers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_list_of_existing_followers);
+    }
+}
