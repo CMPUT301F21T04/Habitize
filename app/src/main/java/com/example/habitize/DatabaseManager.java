025a9e86c060493a71486e5d465738f8fbcde4d9
sfedyk <stasfedyk1911@gmail.com>
2021-11-02 19:57:43 -0600

    added overloaded function for different functionality

--- app/src/main/java/com/example/habitize/DatabaseManager.java
+++ app/src/main/java/com/example/habitize/DatabaseManager.java
@@ -56,9 +56,34 @@ public class DatabaseManager {
                 habitAdapter.notifyDataSetChanged();
             }
         });
+    }
+    // get all the habits and put into a list, but don't update a habitAdapter
+    public void getAllHabits(String user,ArrayList<Habit> recievingList){
 
-
-
+        db.collection("Users").document(user).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                ArrayList<Habit> mappedList =  (ArrayList<Habit>) value.get("habits");
+                recievingList.clear();
+                for(int i = 0; i < mappedList.size() ; i++){ // get each item one by one
+                    Map<String,Object> habitFields = (Map<String, Object>) mappedList.get(i); // map to all the fields
+                    // retrieves all the habit information and adds it to the habitList
+                    String name = (String) habitFields.get("name");
+                    String description = (String) habitFields.get("description");
+                    String date = (String) habitFields.get("startDate");
+                    boolean mondayRec = (boolean) habitFields.get("mondayR");
+                    boolean tuesdayRec = (boolean) habitFields.get("tuesdayR");
+                    boolean wednesdayRec = (boolean) habitFields.get("wednesdayR");
+                    boolean thursdayRec = (boolean) habitFields.get("thursdayR");
+                    boolean fridayRec = (boolean) habitFields.get("fridayR");
+                    boolean saturdayRec = (boolean) habitFields.get("saturdayR");
+                    boolean sundayRec = (boolean) habitFields.get("sundayR");
+                    Habit newHabit = new Habit(name,description, date, mondayRec, tuesdayRec, wednesdayRec,
+                            thursdayRec, fridayRec, saturdayRec, sundayRec); // create a new habit out of this information
+                    recievingList.add(newHabit); // add it to the habitList
+                }
+            }
+        });
     }
 
 
