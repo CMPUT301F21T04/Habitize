eda9a711c4909fa97cd01521bb7ed0d196f7661f
sfedyk <stasfedyk1911@gmail.com>
2021-11-03 20:49:04 -0600

    created registrationlistener to better obey OOP

--- app/src/main/java/com/example/habitize/DatabaseManager.java
+++ app/src/main/java/com/example/habitize/DatabaseManager.java
@@ -1,8 +1,11 @@
 package com.example.habitize;
 
+import android.content.Context;
+
 import androidx.annotation.Nullable;
 
 import com.example.habitize.Habit;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -18,19 +21,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DatabaseManager {
+    interface registrationListener{
+        void continueRegistration();
+    }
 
 
     // db is shared across all instances of the manager
     private static FirebaseFirestore db;
     private static String user;
+    private static registrationListener listener;
     // we initialize the firestore ONCE. Many objects but all will refer to the same instance
     static {
         db = FirebaseFirestore.getInstance();
     }
 
 
-    // does nothing
-    DatabaseManager(){
+    // activity we initialize this in will have to implement the registration and loggin in
+    DatabaseManager(Context context){
+        listener = (registrationListener) context;
     }
 
 
@@ -103,7 +111,14 @@ public class DatabaseManager {
 
 
     // TODO: FInish writing this method
-    public static void createProfileIfUserNotTaken(String username){
+    public static void checkUserNameExistence(String user){
+        db.collection("User").document(user).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                listener.continueRegistration();
+            }
+        });
+
     }
 
 
@@ -113,6 +128,15 @@ public class DatabaseManager {
         db.collection("Users").document(user).update(listMap);
 
     }
+
+    public static void updateHabitsAtDay(String user, ArrayList<Habit> updatedHabits,String day){
+
+    }
+
+    public static boolean userExists(){
+        return true;
+    }
+
 
     // pulls the habits in the list labelled to the weekday.
     // user is the current user. recievingList is the list we want to pull into. habitAdapter is the adapter we want to
