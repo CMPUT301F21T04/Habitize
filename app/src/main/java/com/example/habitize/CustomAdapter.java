febfb357b7d30caf0dd3d4f22752722b8c5247bb
Rachel <45080402+rachelraso@users.noreply.github.com>
2021-11-05 14:05:34 -0600

    Added comments and javadocs

--- app/src/main/java/com/example/habitize/CustomAdapter.java
+++ app/src/main/java/com/example/habitize/CustomAdapter.java
@@ -20,18 +20,34 @@ import java.util.List;
 public class CustomAdapter extends ArrayAdapter<Habit>{
 
     public interface habitViewListener{
+        /**
+         * knows which habit was pressed on in the listview
+         * @param position gets position in list
+         */
         public void viewHabitPressed(int position);
 
     }
+
+
     public interface habitCheckListener{
+        /**
+         * to view a record that was pressed on
+         * @param position gets position in list
+         */
         public void recordEvent(int position);
     }
+
+    //variables to work with
     private habitViewListener viewListener;
     private habitCheckListener checkListener;
     private List<Habit> habits;
     private Context context;
 
-    //
+    /**
+     * constructor to initialize CustomAdapter
+     * @param context context for customAdapter
+     * @param habits where all habits go in a list
+     */
     public CustomAdapter(Context context, List<Habit> habits){
         super(context,0,habits);
         this.habits = habits;
@@ -39,6 +55,14 @@ public class CustomAdapter extends ArrayAdapter<Habit>{
         this.viewListener = (habitViewListener) context;
         this.checkListener = (habitCheckListener) context;
     }
+
+    /**
+     * gets View of list
+     * @param position position in list
+     * @param convertView
+     * @param parent
+     * @return returns the custom list item
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -46,30 +70,34 @@ public class CustomAdapter extends ArrayAdapter<Habit>{
         if(view == null){
             view = LayoutInflater.from(context).inflate(R.layout.list_content,parent,false);
         }
+
         // retrieve habit
         Habit habit = habits.get(position);
         TextView nameField = view.findViewById(R.id.habitName);
         // Setting our custom list items
         nameField.setText(habit.getName());
         Button viewButton = view.findViewById(R.id.viewHabit);
+
+        //when click view button
         viewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // we view the habit at the position
+                //view the habit at the position
                 viewListener.viewHabitPressed(position);
             }
         });
 
+        //when click circular floating action button
         FloatingActionButton completeButton = view.findViewById(R.id.completeHabit);
         completeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //brings you to the record for that list item
                 checkListener.recordEvent(position);
             }
         });
 
         return view;
-
 
     }
 
