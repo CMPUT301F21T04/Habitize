ac7d303da5237544ddd704854453f2fa5ebe85b1
kamillahhasham <kamillah@ualberta.ca>
2021-11-04 21:29:35 -0600

    Added FollowRequests activity and UI file, and hooked up Follow Requests button to launch new activity

---
+++ app/src/main/java/com/example/habitize/FollowRequests.java
@@ -0,0 +1,14 @@
+package com.example.habitize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FollowRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_follow_requests);
+    }
+}
