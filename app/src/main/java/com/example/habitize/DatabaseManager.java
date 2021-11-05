7afdb19e577c620fc731c4df44f5708440cf4563
sfedykua <stasfedyk1911@gmail.com>
2021-11-04 19:23:32 -0600

    rachels database changes

--- app/src/main/java/com/example/habitize/DatabaseManager.java
+++ app/src/main/java/com/example/habitize/DatabaseManager.java
@@ -228,17 +228,27 @@ public class DatabaseManager {
     // pulls the habits in the list labelled to the weekday.
     // user is the current user. recievingList is the list we want to pull into. habitAdapter is the adapter we want to
     // notify when we fill it
-    public static void getTodaysHabits(String user, ArrayList<Habit> recievingList, CustomAdapter habitAdapter) {
-        // we get the date
-        Date date = new Date();
-        String dayWeekText = new SimpleDateFormat("EEEE").format(date);
+    /*
+    public static void getTodaysHabits(String user,ArrayList<Habit> recievingList,CustomAdapter habitAdapter){
+        simpleDateFormat = new SimpleDateFormat("EEEE");
+        Date d = new Date();
+        //gives the day of the week of the user (if today is actually Monday it will say Monday)
+        String dayOfTheWeek = simpleDateFormat.format(d);
+
+//        if(dayOfTheWeek.equals("")){
+//            System.out.println("YESYESYESYESYEYSEYSEEYYESYEYSEYSYE");
+//        }
+//        else{
+//            System.out.println("NONONONONONONONONONONONONOOOOOOOO");
+//        }
+
         db.collection("Users").document(user).addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                ArrayList<Habit> mappedList = (ArrayList<Habit>) value.get(dayWeekText); // retrieving habits scheduled for today
+                ArrayList<Habit> mappedList =  (ArrayList<Habit>) value.get("habits");
                 recievingList.clear();
-                for (int i = 0; i < mappedList.size(); i++) { // get each item one by one
-                    Map<String, Object> habitFields = (Map<String, Object>) mappedList.get(i); // map to all the fields
+                for(int i = 0; i < mappedList.size() ; i++){ // get each item one by one
+                    Map<String,Object> habitFields = (Map<String, Object>) mappedList.get(i); // map to all the fields
                     // retrieves all the habit information and adds it to the habitList
                     String name = (String) habitFields.get("name");
                     String description = (String) habitFields.get("description");
@@ -250,18 +260,38 @@ public class DatabaseManager {
                     boolean fridayRec = (boolean) habitFields.get("fridayR");
                     boolean saturdayRec = (boolean) habitFields.get("saturdayR");
                     boolean sundayRec = (boolean) habitFields.get("sundayR");
-                    Habit newHabit = new Habit(name, description, date, mondayRec, tuesdayRec, wednesdayRec,
+                    Habit newHabit = new Habit(name,description, date, mondayRec, tuesdayRec, wednesdayRec,
                             thursdayRec, fridayRec, saturdayRec, sundayRec); // create a new habit out of this information
-                    recievingList.add(newHabit); // add it to the habitList
+
+                    //recievingList.add(newHabit);
+                    if ((mondayRec = true) && (dayOfTheWeek.equals("Monday"))){
+                        recievingList.add(newHabit); // add it to the habitList
+                    }
+                    if ((tuesdayRec = true) && (dayOfTheWeek.equals("Tuesday"))){
+                        recievingList.add(newHabit);
+                    }
+                    if ((wednesdayRec = true) && (dayOfTheWeek.equals("Wednesday"))){
+                        recievingList.add(newHabit);
+                    }
+                    if ((thursdayRec = true) && (dayOfTheWeek.equals("Thursday"))){
+                        recievingList.add(newHabit);
+                    }
+                    if ((fridayRec = true) && (dayOfTheWeek.equals("Friday"))){
+                        recievingList.add(newHabit);
+                    }
+                    if ((saturdayRec = true) && (dayOfTheWeek.equals("Saturday"))){
+                        recievingList.add(newHabit);
+                    }
+                    if ((sundayRec = true) && (dayOfTheWeek.equals("Sunday"))){
+                        recievingList.add(newHabit);
+                    }
+
                 }
                 habitAdapter.notifyDataSetChanged();
             }
-
         });
-
-
     }
-
+*/
     // the authenticator class will become the listener and be forced to implement procedure
 
     // returns the user's habits if the user is found.
