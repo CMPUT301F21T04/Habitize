712d6d998fd8ab4e5fb5bf729f7550116de75fd0
Shanemel Asuncion <shanemelasuncion@gmail.com>
2021-11-23 20:09:13 -0700

    Added camera functionality

--- app/src/main/java/com/example/habitize/ViewHabitImageFragment.java
+++ app/src/main/java/com/example/habitize/ViewHabitImageFragment.java
@@ -2,6 +2,7 @@ package com.example.habitize;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -20,8 +21,9 @@ import java.io.ByteArrayOutputStream;
 
 public class ViewHabitImageFragment extends Fragment {
     private ImageView imageView;
-    private Button addImageBtn;
+    private Button addImageBtn, viewCamBtn;
     private static final int PICK_IMAGE = 100;
+    private static final int CAM_IMG = 200;
     private Uri imageUri;
     private String imageAddr;
 
@@ -52,6 +54,7 @@ public class ViewHabitImageFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_view_habit_image, container, false);
         imageView = root.findViewById(R.id.FragmentViewHabitNewImage);
         addImageBtn = root.findViewById(R.id.FragmentViewHabitNewImageBtn);
+        viewCamBtn = root.findViewById(R.id.FragmentViewHabitNewCamBtn);
         addImageBtn.setEnabled(false);
 
 
@@ -62,6 +65,10 @@ public class ViewHabitImageFragment extends Fragment {
             }
         });
 
+        viewCamBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openCamera();  }
+        });
 
         DatabaseManager.getAndSetImage(imageAddr,imageView);
         return root;
@@ -70,6 +77,16 @@ public class ViewHabitImageFragment extends Fragment {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
     }
+
+    private void openCamera(){
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try {
+                startActivityForResult(takePictureIntent, CAM_IMG);
+            } catch (ActivityNotFoundException e) {
+                // display error state to the user
+            }
+        }
+
 
     public void setEditable(){
         addImageBtn.setEnabled(true);
@@ -91,7 +108,13 @@ public class ViewHabitImageFragment extends Fragment {
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
         }
+        if(resultCode==RESULT_OK && requestCode == CAM_IMG){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            }
+        }
     }
 
 
-}
+
